91bd2c486fde651398d60afe972b02eec9c3f404
Panni <simaianna3@gmail.com>
2019-02-12 17:21:10 +0100

    json part done

---
+++ js/main.js
@@ -0,0 +1,44 @@
+var header = document.querySelector("header");
+var section = document.querySelector("section");
+
+let requestUrl =
+  "https://gist.githubusercontent.com/LeandroDCI/697427913933937f714dc9c4e728d1c7/raw/ce065f1a72d937eade74e5523d68d834ec2b5304/Heroes.json";
+
+let request = new XMLHttpRequest();
+request.open("GET", requestUrl);
+request.responseType = "json";
+request.send();
+
+request.onload = function() {
+  let superHeroes = request.response;
+  console.log(superHeroes);
+  showHeroes(superHeroes);
+};
+
+function showHeroes(heroes) {
+  const content = heroes
+    .map(function(hero) {
+      const {
+        superhero,
+        publisher,
+        alter_ego,
+        first_appearance,
+        characters
+      } = hero;
+      const charArr = characters.split(",");
+      const charList = charArr.map(character => `<li>${character}</li>`);
+
+      const charReverse = charList.join("");
+      console.log(charReverse);
+      return `
+      <div> 
+    <p>Name: ${superhero}</p>
+    <p>Created by: ${publisher}</p>
+    <p>Alterego: ${alter_ego}</p>
+    <p>First appearance: ${first_appearance}</p>
+    <p>Characters: <ul>${charReverse}</ul></p>
+      </div>`;
+    })
+    .join(" ");
+  section.innerHTML = content;
+}
